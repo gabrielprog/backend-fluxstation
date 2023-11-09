3a2be5227f29e1c1c5be93c4d32908be6f0d8617
Gabriel Santos <69699850+gabrielprog@users.noreply.github.com>
2023-11-09 20:38:43 +0000

    Refactor test and add new test for type unknown

--- src/tests/FuelUseCase.test.js
+++ src/tests/FuelUseCase.test.js
@@ -6,26 +6,28 @@ describe('UseCase', () => {
   
       const result = useCase.calculePriceFuel('gasoline', 10);
   
-      expect(result.price).toBe(50);
+      expect(result).toBe(50);
     });
-});
 
-describe('UseCase', () => {
-  it('should calculate the price of ethanol for 10 liters and return FuelEntity', () => {
-    const useCase = new UseCase();
+    it('should calculate the price of ethanol for 10 liters and return FuelEntity', () => {
+      const useCase = new UseCase();
+  
+      const result = useCase.calculePriceFuel('ethanol', 10);
+  
+      expect(result).toBe(19.2);
+    });
 
-    const result = useCase.calculePriceFuel('ethanol', 10);
+    it('should calculate the price of diesel for 10 liters and return FuelEntity', () => {
+      const useCase = new UseCase();
+  
+      const result = useCase.calculePriceFuel('diesel', 10);
+  
+      expect(result).toBe(35.33);
+    });
 
-    expect(result.price).toBe(19.2);
-  });
-});
-
-describe('UseCase', () => {
-  it('should calculate the price of diesel for 10 liters and return FuelEntity', () => {
-    const useCase = new UseCase();
-
-    const result = useCase.calculePriceFuel('diesel', 10);
-
-    expect(result.price).toBe(35.33);
-  });
+    it('should return a message when an unsupported fuel type is provided', () => {
+      const useCase = new UseCase();
+      const result = useCase.calculePriceFuel('unknownFuelType', 10);
+      expect(result.message).toBe('This fuel type is not supported yet.');
+    });
 });
