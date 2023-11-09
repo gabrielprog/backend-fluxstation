8f3200f51d03814013722cff4ce1e80dba6972b2
Gabriel Santos <69699850+gabrielprog@users.noreply.github.com>
2023-11-09 18:28:24 +0000

    Add json suport

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-const express = require('express')
-const app = express()
-const port = 8080
+const express = require('express');
+var bodyParser = require('body-parser');
+const app = express();
+const port = 8080;
 
 const routes = require('./routers');
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(routes);
 
+
 app.listen(port, () => {
-  console.log(`App listening on port ${port}`)
-})
+  console.log(`App listening on port ${port}`);
+});
