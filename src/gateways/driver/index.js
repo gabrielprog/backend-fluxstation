295a1e514afd5d5ccdb7f775f97ecb18a608cbe8
Gabriel Santos <69699850+gabrielprog@users.noreply.github.com>
2023-11-09 18:14:17 +0000

    Add exception capture

--- src/gateways/driver/index.js
+++ src/gateways/driver/index.js
@@ -4,20 +4,32 @@ const { v4: uuidv4 } = require('uuid');
 
 class DriverGateway {
     async create({ body }) {
-        
         const {name: Name, email} = body;
+        try {
+            const driverModel = new DriverRecord(sequelize);
+            const { id, name } = await driverModel.DriverModel.create({
+                id: uuidv4(),
+                email: email,
+                name: Name
+            });
 
-        const driverModel = new DriverRecord(sequelize);
-        const { id, name } = await driverModel.DriverModel.create({
-            id: uuidv4(),
-            email: email,
-            name: Name
-        });
+            return {
+                driver: {
+                    id, 
+                    name 
+                },
+                status: 200
+            };
 
-        return {
-            id, 
-            name 
-        };
+        }catch (error) {
+            console.error('Erro ao criar registro de motorista:', error);
+    
+            return { 
+                driver: 'Erro ao criar registro de motorista',
+                status: 500
+            };
+
+        }
     }
 }
 
