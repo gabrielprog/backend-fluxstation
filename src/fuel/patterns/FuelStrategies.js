3b90eac06efc88d92606730531ab6532ce24120b
Gabriel Santos <69699850+gabrielprog@users.noreply.github.com>
2023-11-09 01:14:04 +0000

    Pattern strategy

---
+++ src/fuel/patterns/FuelStrategies.js
@@ -0,0 +1,15 @@
+class FuelStrategies {
+    static gasoline() {
+      return 5;
+    }
+  
+    static ethanol() {
+      return 1.92;
+    }
+  
+    static diesel() {
+      return 3.533;
+    }
+}
+
+module.exports = FuelStrategies;
