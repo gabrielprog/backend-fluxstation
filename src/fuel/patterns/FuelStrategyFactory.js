60f2d9702b13c41ce712c5b1e6b171b935923c7b
Gabriel Santos <69699850+gabrielprog@users.noreply.github.com>
2023-11-09 20:34:41 +0000

    Refactor

--- src/fuel/patterns/FuelStrategyFactory.js
+++ src/fuel/patterns/FuelStrategyFactory.js
@@ -2,7 +2,11 @@ const FuelStrategies = require('./FuelStrategies');
 
 class FuelStrategyFactory {
     static createStrategy(type) {
-      return FuelStrategies[type] || null;
+      if(type == "gasoline") return FuelStrategies.gasoline;
+
+      if(type == "ethanol") return FuelStrategies.ethanol;
+
+      if(type == "diesel") return FuelStrategies.diesel;
     }
 }
 
