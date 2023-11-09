59a212c5b97e1bd8d893d664b67c62e939e76b55
Gabriel Santos <69699850+gabrielprog@users.noreply.github.com>
2023-11-09 20:12:48 +0000

    Create router for create fuel

--- src/routers.js
+++ src/routers.js
@@ -5,6 +5,7 @@ const FuelController = require("./controllers/fuel");
 const DriverController = require("./controllers/driver");
 
 router.get('/fuel', FuelController.read)
+router.post('/fuel', FuelController.create)
 
 router.post('/driver', DriverController.create)
 
