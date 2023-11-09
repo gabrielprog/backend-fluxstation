68beaf5de0ed82ce3ac9f0bd6ddf9b08b09c9e66
Gabriel Santos <69699850+gabrielprog@users.noreply.github.com>
2023-11-09 21:05:20 +0000

    Add Validation

--- src/controllers/driver/index.js
+++ src/controllers/driver/index.js
@@ -1,8 +1,21 @@
 const DriverGateway = require("../../gateways/driver/");
+const { object, string } = require('yup');
 
 class DriverController {
     async create(request, response) {
-        const recordsDriver = await DriverGateway.create(request)
+
+        const schema = object().shape({
+            name: string().required('O campo "name" é obrigatório'),
+            email: string().email('O campo "email" não é um email válido').required('O campo "email" é obrigatório')
+        });
+
+        try {
+            await schema.validate(request.body, { abortEarly: false });
+        } catch (error) {
+            return response.status(400).json({ error: error.errors });
+        }
+        
+        const recordsDriver = await DriverGateway.create(request);
 
         return response.status(recordsDriver.status).json({
             driver: recordsDriver.driver
--- src/controllers/fuel/index.js
+++ src/controllers/fuel/index.js
@@ -1,3 +1,4 @@
+const { object, string, number, mixed } = require('yup');
 const FuelGateway = require('../../gateways/fuel/');
 const UseCaseFuel = require('../../fuel/useCases/UseCase');
 
@@ -19,12 +20,26 @@ class FuelController {
     }
 
     async create(request, response) {
+        const schema = object().shape({
+            driverId: string()
+              .uuid('O campo "driverId" deve ser um UUID válido')
+              .required('O campo "driverId" é obrigatório'),
+      
+            quantityLiters: number()
+              .positive('O campo "quantityLiters" deve ser um número positivo')
+              .required('O campo "quantityLiters" é obrigatório'),
+      
+            fuelType: mixed()
+              .oneOf(['Gasoline', 'Diesel', 'Ethanol'], 'O campo "fuelType" deve ser um dos valores permitidos: Gasoline, Diesel, Ethanol')
+              .required('O campo "fuelType" é obrigatório'),
+        });
 
-        if (request.body.driverId === undefined) {
-            return response.status(400).json({ 
-                fuel: 'Campo "id" não foi fornecido ou é indefinido.',
-            });
+        try {
+            await schema.validate(request.body, { abortEarly: false });
+        } catch (validationError) {
+            return response.status(400).json({ error: validationError.errors });
         }
+
         const useCases = new UseCaseFuel();
         request.body.totalPrice = useCases.calculePriceFuel(request.body.fuelType.toLowerCase(), request.body.quantityLiters);
 
