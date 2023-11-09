f2f0b6f50bc039db1ee80818bcedb44704db9efa
Gabriel Santos <69699850+gabrielprog@users.noreply.github.com>
2023-11-09 12:35:51 +0000

    Add layer routers

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ const express = require('express')
 const app = express()
 const port = 8080
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const routes = require('./routers');
+
+app.use(routes);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`App listening on port ${port}`)
 })
