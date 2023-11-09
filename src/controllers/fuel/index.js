2b7fa5190b090f610340f7875a34be1943496257
Gabriel Santos <69699850+gabrielprog@users.noreply.github.com>
2023-11-09 20:33:38 +0000

    Create Fuel store

--- src/controllers/fuel/index.js
+++ src/controllers/fuel/index.js
@@ -1,4 +1,5 @@
-const FuelGateway = require("../../gateways/fuel/");
+const FuelGateway = require('../../gateways/fuel/');
+const UseCaseFuel = require('../../fuel/useCases/UseCase');
 
 class FuelController {
 
@@ -11,6 +12,23 @@ class FuelController {
         }
 
         const recordsFuel = await FuelGateway.read(request)
+
+        return response.status(recordsFuel.status).json({
+            fuel: recordsFuel.fuel
+        });
+    }
+
+    async create(request, response) {
+
+        if (request.body.driverId === undefined) {
+            return response.status(400).json({ 
+                fuel: 'Campo "id" não foi fornecido ou é indefinido.',
+            });
+        }
+        const useCases = new UseCaseFuel();
+        request.body.totalPrice = useCases.calculePriceFuel(request.body.fuelType.toLowerCase(), request.body.quantityLiters);
+
+        const recordsFuel = await FuelGateway.create(request)
 
         return response.status(recordsFuel.status).json({
             fuel: recordsFuel.fuel
