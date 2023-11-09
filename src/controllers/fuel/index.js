dade015391f3017c3819d0ff5ac6c94697aa6479
Gabriel Santos <69699850+gabrielprog@users.noreply.github.com>
2023-11-09 17:31:00 +0000

    Create read controller fuel

--- src/controllers/fuel/index.js
+++ src/controllers/fuel/index.js
@@ -3,6 +3,13 @@ const FuelGateway = require("../../gateways/fuel/");
 class FuelController {
 
     async read(request, response) {
+
+        if (request.query.id === undefined) {
+            return response.status(400).json({ 
+                fuel: 'Parâmetro "id" não foi fornecido ou é indefinido.',
+            });
+        }
+
         const recordsFuel = await FuelGateway.read(request)
 
         return response.status(recordsFuel.status).json({
